43455245da84b9f936b23ff7b6f23eca87f7e438
jawmeschege <jawiwy@gmail.com>
2022-04-08 01:38:17 +0400

    swap

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -54,7 +54,7 @@ class MyDocument extends Document {
       
           <link rel="icon" type="image/png" sizes="16x16" href="images/favicon.png"/>
           <link href="vendor/bootstrap-select/dist/css/bootstrap-select.min.css" rel="stylesheet" />
-          <link href="/css/style.css" rel="stylesheet"/>
+          <link href="/utils/css/style.css" rel="stylesheet"/>
           <link href="https://cdn.lineicons.com/2.0/LineIcons.css" rel="stylesheet"/>
           <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css"
             integrity="sha512-Fo3rlrZj/k7ujTnHg4CGR2D7kSs0v4LLanw2qksYuRlEzO+tcaEPQogQ0KaoGN26/zrn20ImR1DfuLWnOo7aBA=="
