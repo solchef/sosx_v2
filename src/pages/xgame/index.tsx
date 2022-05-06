38a1fd26de7169da123cefff342af475ea23826f
jawmeschege <jawiwy@gmail.com>
2022-05-06 22:30:25 +0400

    pushing

--- src/pages/xgame/index.tsx
+++ src/pages/xgame/index.tsx
@@ -5,3 +5,4 @@ const xgame = () => {
 }
 
 export default xgame;
+  
--- src/utils/contractHelpers.ts
+++ src/utils/contractHelpers.ts
@@ -2,113 +2,30 @@ import type { Signer } from '@ethersproject/abstract-signer'
 import type { Provider } from '@ethersproject/providers'
 import { Contract } from '@ethersproject/contracts'
 import { simpleRpcProvider } from 'utils/providers'
-import poolsConfig from 'config/constants/pools'
-import { PoolCategory } from 'config/constants/types'
 import tokens from 'config/constants/tokens'
 
 // Addresses
 import {
-  getAddress,
   getPancakeProfileAddress,
-  getPancakeRabbitsAddress,
-  getBunnyFactoryAddress,
-  getBunnySpecialAddress,
-  getLotteryV2Address,
   getMasterChefAddress,
   getPointCenterIfoAddress,
-  getClaimRefundAddress,
-  getTradingCompetitionAddress,
-  getEasterNftAddress,
-  getCakeVaultAddress,
-  getIfoPoolAddress,
   getstakingContractAddress,
   getPredictionsAddress,
-  getChainlinkOracleAddress,
-  getMulticallAddress,
-  getBunnySpecialCakeVaultAddress,
-  getBunnySpecialPredictionAddress,
-  getBunnySpecialLotteryAddress,
-  getFarmAuctionAddress,
-  getAnniversaryAchievement,
-  getNftMarketAddress,
-  getNftSaleAddress,
-  getPancakeSquadAddress,
-  getTradingCompetitionAddressV2,
-  getBunnySpecialXmasAddress,
 } from 'utils/addressHelpers'
 
 // ABI
-import profileABI from 'config/abi/pancakeProfile.json'
-import pancakeRabbitsAbi from 'config/abi/pancakeRabbits.json'
-import bunnyFactoryAbi from 'config/abi/bunnyFactory.json'
-import bunnySpecialAbi from 'config/abi/bunnySpecial.json'
-import bep20Abi from 'config/abi/erc20.json'
-import erc721Abi from 'config/abi/erc721.json'
-import lpTokenAbi from 'config/abi/lpToken.json'
 import cakeAbi from 'config/abi/cake.json'
-import ifoV1Abi from 'config/abi/ifoV1.json'
-import ifoV2Abi from 'config/abi/ifoV2.json'
 import pointCenterIfo from 'config/abi/pointCenterIfo.json'
-import lotteryV2Abi from 'config/abi/lotteryV2.json'
 import masterChef from 'config/abi/masterchef.json'
-import sousChef from 'config/abi/sousChef.json'
-import sousChefV2 from 'config/abi/sousChefV2.json'
-import sousChefBnb from 'config/abi/sousChefBnb.json'
-import claimRefundAbi from 'config/abi/claimRefund.json'
-import tradingCompetitionAbi from 'config/abi/tradingCompetition.json'
-import tradingCompetitionV2Abi from 'config/abi/tradingCompetitionV2.json'
-import easterNftAbi from 'config/abi/easterNft.json'
-import cakeVaultAbi from 'config/abi/cakeVault.json'
-import ifoPoolAbi from 'config/abi/ifoPool.json'
 import sosxStakingAbi from 'config/abi/sosxABI.json'
 import predictionsAbi from 'config/abi/predictions.json'
-import chainlinkOracleAbi from 'config/abi/chainlinkOracle.json'
-import MultiCallAbi from 'config/abi/Multicall.json'
-import bunnySpecialCakeVaultAbi from 'config/abi/bunnySpecialCakeVault.json'
-import bunnySpecialPredictionAbi from 'config/abi/bunnySpecialPrediction.json'
-import bunnySpecialLotteryAbi from 'config/abi/bunnySpecialLottery.json'
-import bunnySpecialXmasAbi from 'config/abi/bunnySpecialXmas.json'
-import farmAuctionAbi from 'config/abi/farmAuction.json'
-import anniversaryAchievementAbi from 'config/abi/anniversaryAchievement.json'
-import nftMarketAbi from 'config/abi/nftMarket.json'
-import nftSaleAbi from 'config/abi/nftSale.json'
-import pancakeSquadAbi from 'config/abi/pancakeSquad.json'
-import erc721CollectionAbi from 'config/abi/erc721collection.json'
 
 // Types
 import type {
-  ChainlinkOracle,
-  FarmAuction,
   Predictions,
-  AnniversaryAchievement,
-  IfoV1,
-  IfoV2,
-  IfoPool,
-  Erc20,
-  Erc721,
   Cake,
-  BunnyFactory,
-  PancakeRabbits,
   PancakeProfile,
-  LotteryV2,
   Masterchef,
-  SousChef,
-  SousChefV2,
-  BunnySpecial,
-  LpToken,
-  ClaimRefund,
-  TradingCompetition,
-  TradingCompetitionV2,
-  EasterNft,
-  CakeVault,
-  Multicall,
-  BunnySpecialCakeVault,
-  BunnySpecialPrediction,
-  BunnySpecialLottery,
-  NftMarket,
-  NftSale,
-  PancakeSquad,
-  Erc721collection,
   PointCenterIfo,
   Staking,
 } from 'config/abi/types'
@@ -137,8 +54,6 @@ export const getProfileContract = (signer?: Signer | Provider) => {
 export const getMasterchefContract = (signer?: Signer | Provider) => {
   return getContract(masterChef, getMasterChefAddress(), signer) as Masterchef
 }
-
-
 
 
 
