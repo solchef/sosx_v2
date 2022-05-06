3296f40208e90a70854f413ba4c2ad88323ac2ba
SRB <98799040+sruthyroseboban@users.noreply.github.com>
2022-05-06 21:09:10 +0400

    challenges issue

--- src/views/Games/Votechallenge/index.tsx
+++ src/views/Games/Votechallenge/index.tsx
@@ -2,6 +2,7 @@ import Link from "next/link";
 import { useEffect, useState } from "react";
 import { create } from "ipfs-http-client";
 import { concat } from "uint8arrays";
+import { exit } from "process";
 
 const server = create({
   url: "http://127.0.0.1:5001",
@@ -9,12 +10,12 @@ const server = create({
 
 export default function Votechallenge() {
   const [challenges, setChallenges] = useState<any[]>([]);
-  const [votesCount, setVotesCount] = useState<any[]>([]);
+//   const [votesCount, setVotesCount] = useState<any>();
   const obj = {};
 
-  challenges.forEach((element, index) => {
-	obj[element] = votesCount[index];
-  });
+//   challenges.forEach((element, index) => {
+// 	obj[element] = votesCount[index];
+//   });
 //   const [content, setContent] = useState({first:[], second:[]});
   let challengedata = [];
   let countedata = [];
@@ -22,51 +23,44 @@ export default function Votechallenge() {
   useEffect(() => {
     const getData = async () => {
 		// console.log('st',await server.files.stat('/'))
+	let challenges = [];
       for await (const resultPart of server.files.ls("/")) {
-        for await (const cha of server.files.ls(`/${resultPart.name}`)) {
+		let challenge;
+		let vote;
+
+		for await (const cha of server.files.ls(`/${resultPart.name}`)) {
+			// let files = server.files.ls(`/${resultPart.name}`);
+			// console.log(cha);
 			const chunks = [];
 			if (cha.name == 'votes') {
-				const votesNum = await (await server.files.stat(`/${resultPart.name}/votes`)).blocks
-			let myObj={
-				"vote":votesNum
+				let votes = await server.files.stat(`/${resultPart.name}/votes`)
+				vote = votes.blocks;
+			} 
+
+			if (cha.name == 'challenge.json') {
+			 for await (const chunk of server.cat(cha.cid)) {
+				chunks.push(chunk);
+				}
+				const data = concat(chunks);
+				challenge = JSON.parse(
+				new TextDecoder().decode(data).toString()
+				);
+			 }
+
 			}
-			countedata.push(myObj)
+
+			let challengeData = {
+				challenge: challenge,
+				votes: vote
 			}
+			challenges.push(challengeData);
+		}
 
-          if (cha.name == "challenge.json") {
-            
-            for await (const chunk of server.cat(cha.cid)) {
-              chunks.push(chunk);
-            }
-
-            const data = concat(chunks);
-            // console.log(data)
-
-            const decodedData = JSON.parse(
-              new TextDecoder().decode(data).toString()
-            );
-			const wit = JSON.stringify({decodedData})
-			
-			
-
-            // console.log(decodedData)
-            challengedata.push(decodedData);
-            console.log(challengedata);
-            console.log("ch", challenges);
-          }
-		  
-		 
-
-
-        }
-      }
-	  
-	//   setContent({first:challengedata, second:countedata})
-	setChallenges(challengedata);
-	setVotesCount(countedata);
+	    setChallenges(challenges);
     };
     getData();
   }, []);
+  
   console.log("challenges", challenges);
   return (
     <>
@@ -79,12 +73,12 @@ export default function Votechallenge() {
                 <div className="card-header align-items-start border-0">
                   <div>
                     <span className="fs-12 font-weight-bold success">
-                      {camp.payload.metadata.strategies[0].params.address}
+                      {camp.challenge.payload.metadata.strategies[0].params.address}
                     </span>
-                    <h4 className="fs-18 mb-0 pb-2">{camp.payload.name}</h4>
-                    <span className="fs-12">{camp.payload.body}</span>
+                    <h4 className="fs-18 mb-0 pb-2">{camp.challenge.payload.name}</h4>
+                    <span className="fs-12">{camp.challenge.payload.body}</span>
                     <h4 className="fs-12 text-white pt-3">Rules</h4>
-                    {camp.payload.choices.map((element) => (
+                    {camp.challenge.payload.choices.map((element) => (
                       <ul className="fs-12">
                         <li>
                           <i className="fa-solid fa-check pr-2"></i>
@@ -99,7 +93,7 @@ export default function Votechallenge() {
                   <div>
                     <i className="fa-regular fa-heart p-2"></i>
                     <span className="fs-12 p-1" id="votes">
-                     00
+					{camp.votes}
                     </span>
                     <span className="fs-12">Votes</span>
                   </div>
