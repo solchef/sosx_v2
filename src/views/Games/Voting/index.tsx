afaffd7d4697e732fc7b289eb2cdb893a40b7e31
Fanitrade <fanitrade@Fanitrades-iMac.local>
2022-05-06 17:09:06 +0400

    fix designs

--- src/views/Games/Voting/index.tsx
+++ src/views/Games/Voting/index.tsx
@@ -5,7 +5,7 @@ export default function Voting() {
     return (
         <div className="voting container-fluid">
             <div className="row">
-                <div className="col-8">
+                <div className="col-9">
                     <div className='text-muted font-weight-bold h4'>
                         <h1 className='h1 font-weight-bold'>CAKE Staking Proposal</h1>
 
@@ -58,48 +58,30 @@ export default function Voting() {
 
 
                 </div>
-                <div className="col-4">
+                <div className="col-3">
                     <div className="row">
                         <div className="card border col-12">
-                            <h5 className="card-header font-weight-bold ">Information</h5>
-                            <div className="card-body">
+                            <h5 className=" border-bottom font-weight-bold p-1">Information</h5>
+                          
 
-                                <table className="table font-weight-bold ">
-                                    <tbody>
+                               <div className="row p-1">
+                                   <div className="col-6">Strategie(s)	</div>
+                                   <div className="col-6">Jacob 	</div>
+                               </div>
+                               <div className="row p-1">
+                                   <div className="col-6">Strategie(s)	</div>
+                                   <div className="col-6">Jacob 	</div>
+                               </div>
+                               <div className="row p-1">
+                                   <div className="col-6">Strategie(s)	</div>
+                                   <div className="col-6">Jacob 	</div>
+                               </div>
+                               <div className="row p-1">
+                                   <div className="col-6">Strategie(s)	</div>
+                                   <div className="col-6">Jacob 	</div>
+                               </div>
 
-                                        <tr >
-                                            <th className='border-bottom-0' scope="row">Strategie(s)</th>
-                                            <td className='text-white text-right border-bottom-0'>Jacob</td>
-                                        </tr>
-                                        <tr >
-                                            <th className='border-bottom-0' scope="row">IPFS</th>
-                                            <td className='text-white text-right border-bottom-0'>#Qmdmrk5</td>
-                                        </tr>
-                                        <tr >
-                                            <th className='border-bottom-0' scope="row">Voting system</th>
-                                            <td className='text-white text-right border-bottom-0'>May 5, 2022, 11:00 AM</td>
-                                        </tr>
-                                        <tr >
-                                            <th className='border-bottom-0' scope="row">Start date</th>
-                                            <td className='text-white text-right border-bottom-0'>May 5, 2022, 11:00 AM
-                                            </td>
-                                        </tr>
-                                        <tr >
-                                            <th className='border-bottom-0' scope="row">End date</th>
-                                            <td className='text-white text-right border-bottom-0'>May 5, 2022, 11:00 AM
-                                            </td>
-                                        </tr>
-                                        <tr >
-                                            <th className='border-bottom-0' scope="row">Snapshot</th>
-                                            <td className='text-white text-right border-bottom-0'>May 5, 2022, 11:00 AM
-                                            </td>
-                                        </tr>
-                                    </tbody>
-
-
-                                </table>
-
-                            </div>
+                       
                         </div>
                     </div>
                     <div className="row pt-5">
