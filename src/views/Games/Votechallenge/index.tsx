b8c47474b3c8269113fd8263b04ba4df710700c0
Omar Elweshy <37350174+omarelweshy@users.noreply.github.com>
2022-05-07 19:50:23 +0400

    add new sytle for voteChallenge

--- src/views/Games/Votechallenge/index.tsx
+++ src/views/Games/Votechallenge/index.tsx
@@ -1,12 +1,109 @@
-<<<<<<< HEAD
-import Link from 'next/link'
+import Link from "next/link";
+import { useEffect, useState } from "react";
+import { create } from "ipfs-http-client";
+import { concat } from "uint8arrays";
+
+const server = create({
+  url: "http://127.0.0.1:5001",
+});
 
 export default function Votechallenge() {
+  const [challenges, setChallenges] = useState<any[]>([]);
+  const obj = {};
 
-	return (
-		<>
+  useEffect(() => {
+    const getData = async () => {
+	let challenges = [];
+      for await (const resultPart of server.files.ls("/")) {
+		let challenge;
+		let vote;
 
-			<div className="container-fluid pt-3">
+		for await (const cha of server.files.ls(`/${resultPart.name}`)) {
+			const chunks = [];
+			if (cha.name == 'votes') {
+				let votes = await server.files.stat(`/${resultPart.name}/votes`)
+				vote = votes.blocks;
+			} 
+
+			if (cha.name == 'challenge.json') {
+			 for await (const chunk of server.cat(cha.cid)) {
+				chunks.push(chunk);
+				}
+				const data = concat(chunks);
+				challenge = JSON.parse(
+				new TextDecoder().decode(data).toString()
+				);
+			 }
+			}
+
+			let challengeData = {
+				challenge: challenge,
+				votes: vote
+			}
+			challenges.push(challengeData);
+		}
+
+	    setChallenges(challenges);
+    };
+    getData();
+  }, []);
+  
+  console.log("challenges", challenges);
+  return (
+    <>
+      <div className="container-fluid pt-3">
+        <div className="row pt-3">
+          {challenges.map((camp) => (
+			
+            <div className="col-xl-4 col-md-6">
+              <div className="card overflow-hidden">
+                <div className="card-header align-items-start border-0">
+                  <div>
+                    <span className="fs-12 font-weight-bold success">
+                      {camp.challenge.payload.metadata.strategies[0].params.address}
+                    </span>
+                    <h4 className="fs-18 mb-0 pb-2">{camp.challenge.payload.name}</h4>
+                    <span className="fs-12">{camp.challenge.payload.body}</span>
+                    <h4 className="fs-12 text-white pt-3">Rules</h4>
+                    {camp.challenge.payload.choices.map((element) => (
+                      <ul className="fs-12">
+                        <li>
+                          <i className="fa-solid fa-check pr-2"></i>
+                          {element}
+                        </li>
+                      </ul>
+                    ))}
+                  </div>
+                </div>
+				
+                <div className="card-footer d-flex flex-column flex-lg-row align-items-md-center align-items-start justify-content-between">
+                  <div>
+                    <i className="fa-regular fa-heart p-2"></i>
+                    <span className="fs-12 p-1" id="votes">
+					          {camp.votes}
+                    </span>
+                    <span className="fs-12">Votes</span>
+                  </div>
+                  <Link href={`/challenge/${camp.challenge.payload.name}`}>
+                  <button type="button" className="btn btn-primary ">
+                    <i className="fa-solid fa-check-to-slot pr-2"></i>Details
+                  </button>
+                  </Link>
+                </div>
+			
+              </div>
+            </div>
+		  ))}
+        </div>
+      </div>
+    </>
+  );
+}
+
+
+
+
+{/* <div className="container-fluid pt-3">
 			<div className="App">
     </div>
 				<div className="row pt-3">
@@ -183,108 +280,4 @@ export default function Votechallenge() {
 						<div className="card overflow-hidden p-0">
 							<div className="card-header align-items-start border-0">
 								<div>
-									<span className="fs-12 font-weight-bold success">@challengecreator-1</span>
-=======
-import Link from "next/link";
-import { useEffect, useState } from "react";
-import { create } from "ipfs-http-client";
-import { concat } from "uint8arrays";
-
-const server = create({
-  url: "http://127.0.0.1:5001",
-});
-
-export default function Votechallenge() {
-  const [challenges, setChallenges] = useState<any[]>([]);
-  const obj = {};
-
-  useEffect(() => {
-    const getData = async () => {
-	let challenges = [];
-      for await (const resultPart of server.files.ls("/")) {
-		let challenge;
-		let vote;
-
-		for await (const cha of server.files.ls(`/${resultPart.name}`)) {
-			const chunks = [];
-			if (cha.name == 'votes') {
-				let votes = await server.files.stat(`/${resultPart.name}/votes`)
-				vote = votes.blocks;
-			} 
-
-			if (cha.name == 'challenge.json') {
-			 for await (const chunk of server.cat(cha.cid)) {
-				chunks.push(chunk);
-				}
-				const data = concat(chunks);
-				challenge = JSON.parse(
-				new TextDecoder().decode(data).toString()
-				);
-			 }
->>>>>>> 2628ba9c72dd443025a995e0488c67507a6c804d
-
-			}
-
-			let challengeData = {
-				challenge: challenge,
-				votes: vote
-			}
-			challenges.push(challengeData);
-		}
-
-	    setChallenges(challenges);
-    };
-    getData();
-  }, []);
-  
-  console.log("challenges", challenges);
-  return (
-    <>
-      <div className="container-fluid pt-3">
-        <div className="row pt-3">
-          {challenges.map((camp) => (
-			
-            <div className="col-xl-4 col-md-6">
-              <div className="card overflow-hidden">
-                <div className="card-header align-items-start border-0">
-                  <div>
-                    <span className="fs-12 font-weight-bold success">
-                      {camp.challenge.payload.metadata.strategies[0].params.address}
-                    </span>
-                    <h4 className="fs-18 mb-0 pb-2">{camp.challenge.payload.name}</h4>
-                    <span className="fs-12">{camp.challenge.payload.body}</span>
-                    <h4 className="fs-12 text-white pt-3">Rules</h4>
-                    {camp.challenge.payload.choices.map((element) => (
-                      <ul className="fs-12">
-                        <li>
-                          <i className="fa-solid fa-check pr-2"></i>
-                          {element}
-                        </li>
-                      </ul>
-                    ))}
-                  </div>
-                </div>
-				
-                <div className="card-footer d-flex flex-column flex-lg-row align-items-md-center align-items-start justify-content-between">
-                  <div>
-                    <i className="fa-regular fa-heart p-2"></i>
-                    <span className="fs-12 p-1" id="votes">
-					          {camp.votes}
-                    </span>
-                    <span className="fs-12">Votes</span>
-                  </div>
-                  <Link href={`/challenge/${camp.challenge.payload.name}`}>
-                  <button type="button" className="btn btn-primary ">
-                    <i className="fa-solid fa-check-to-slot pr-2"></i>Details
-                  </button>
-                  </Link>
-                </div>
-			
-              </div>
-            </div>
-		  ))}
-        </div>
-      </div>
-    </>
-  );
-}
+									<span className="fs-12 font-weight-bold success">@challengecreator-1</span> */}
