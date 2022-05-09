fe9866cfdd3aca81a1cc8f414de91d9f38e6599a
Omar Elweshy <37350174+omarelweshy@users.noreply.github.com>
2022-05-09 11:44:26 +0400

    fix address leanth & check for chanllenge

--- src/views/Games/Votechallenge/index.tsx
+++ src/views/Games/Votechallenge/index.tsx
@@ -53,47 +53,56 @@ export default function Votechallenge() {
 <>
       <div className="container-fluid pt-3">
         <div className="row pt-3">
-          {challenges.map((camp) => (
-			
-            <div className="col-xl-4 col-md-6">
-              <div className="card overflow-hidden">
-                <div className="card-header align-items-start border-0">
-                  <div>
-                    <span className="fs-12 font-weight-bold success">
-                      {camp.challenge.payload.metadata.strategies[0].params.address}
-                    </span>
-                    <h4 className="fs-18 mb-0 pb-2">{camp.challenge.payload.name}</h4>
-                    <span className="fs-12">{camp.challenge.payload.body}</span>
-                    <h4 className="fs-12 text-white pt-3">Rules</h4>
-                    {camp.challenge.payload.choices.map((element) => (
-                      <ul className="fs-12">
-                        <li>
-                          <i className="fa-solid fa-check pr-2"></i>
-                          {element}
-                        </li>
-                      </ul>
-                    ))}
-                  </div>
-                </div>
-				
-                <div className="card-footer d-flex flex-column flex-lg-row align-items-md-center align-items-start justify-content-between">
-                  <div>
-                    <i className="fa-regular fa-heart p-2"></i>
-                    <span className="fs-12 p-1" id="votes">
-					          {camp.votes}
-                    </span>
-                    <span className="fs-12">Votes</span>
-                  </div>
-                  <Link href={`/challenge/${camp.challenge.payload.name}`}>
-                  <button type="button" className="btn btn-primary ">
-                    <i className="fa-solid fa-check-to-slot pr-2"></i>Details
-                  </button>
-                  </Link>
-                </div>
-			
-              </div>
-            </div>
-		  ))}
+          {challenges.length > 0 ? (
+            <>
+              {challenges.map((camp) => (
+                    
+                    <div className="col-xl-4 col-md-6">
+                      <div className="card overflow-hidden">
+                        <div className="card-header align-items-start border-0">
+                          <div>
+                            <span className="fs-12 font-weight-bold success">
+                              {camp.challenge.payload.metadata.strategies[0].params.address}
+                            </span>
+                            <h4 className="fs-18 mb-0 pb-2">{camp.challenge.payload.name}</h4>
+                            <span className="fs-12">{camp.challenge.payload.body}</span>
+                            <h4 className="fs-12 text-white pt-3">Rules</h4>
+                            {camp.challenge.payload.choices.map((element) => (
+                              <ul className="fs-12">
+                                <li>
+                                  <i className="fa-solid fa-check pr-2"></i>
+                                  {element}
+                                </li>
+                              </ul>
+                            ))}
+                          </div>
+                        </div>
+                
+                        <div className="card-footer d-flex flex-column flex-lg-row align-items-md-center align-items-start justify-content-between">
+                          <div>
+                            <i className="fa-regular fa-heart p-2"></i>
+                            <span className="fs-12 p-1" id="votes">
+                            {camp.votes}
+                            </span>
+                            <span className="fs-12">Votes</span>
+                          </div>
+                          <Link href={`/challenge/${camp.challenge.payload.name}`}>
+                          <button type="button" className="btn btn-primary ">
+                            <i className="fa-solid fa-check-to-slot pr-2"></i>Details
+                          </button>
+                          </Link>
+                        </div>
+
+                      </div>
+                    </div>
+                ))}
+            </>
+          ) : (
+            <>
+            <div>No challenges</div>
+            </>
+          )}
+          
         </div>
       </div>
     </>
