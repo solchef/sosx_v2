1823e510ac2d4e35aa24857e657f973ba0302cdc
Omar Elweshy <37350174+omarelweshy@users.noreply.github.com>
2022-05-09 10:50:49 +0400

    fix vote ui

--- src/views/Games/Votechallenge/index.tsx
+++ src/views/Games/Votechallenge/index.tsx
@@ -1,245 +1,56 @@
-<<<<<<< HEAD
 import Link from 'next/link'
-
-export default function Votechallenge() {
-
-	return (
-		<>
-
-			<div className="container-fluid pt-3">
-			<div className="App">
-    </div>
-				<div className="row pt-3">
-	
-					<div className="col-xl-4 col-md-6">
-						<div className="card overflow-hidden p-0">
-							<div className="card-header align-items-start border-0">
-								<div>
-									<span className="fs-12 font-weight-bold success">@challengecreator-1</span>
-
-									<h4 className="fs-18 mb-0 pb-2">Challenge Title</h4>
-									<span className="fs-12">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-										eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum
-										suspendisse ultrices gravida. </span>
-									<h4 className="fs-12 text-white pt-3">Rules</h4>
-									<ul className="fs-12">
-										<li><i className="fa-solid fa-check pr-2"></i>Lorem ipsum dolor sit amet.</li>
-										<li><i className="fa-solid fa-check pr-2"></i>Lorem ipsum dolor sit amet.</li>
-									</ul>
-								</div>
-							</div>
-							<div className="card-footer d-flex flex-column flex-lg-row align-items-md-center align-items-start justify-content-between">
-								<div>
-									<i className="fa-regular fa-heart p-2"></i><span className="fs-12 p-1"
-										id="votes">17</span><span className="fs-12">Votes</span>
-								</div>
-								<button type="button"
-									className="btn btn-primary "><i
-										className="fa-solid fa-check-to-slot pr-2"></i>Votte</button>
-							</div>
-						</div>
-					</div>
-					<div className="col-xl-4 col-md-6">
-						<div className="card overflow-hidden p-0">
-							<div className="card-header align-items-start border-0">
-								<div>
-									<span className="fs-12 font-weight-bold success">@challengecreator-1</span>
-
-									<h4 className="fs-18 mb-0 pb-2">Challenge Title</h4>
-									<span className="fs-12">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-										eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum
-										suspendisse ultrices gravida. </span>
-									<h4 className="fs-12 text-white pt-3">Rules</h4>
-									<ul className="fs-12">
-										<li><i className="fa-solid fa-check pr-2"></i>Lorem ipsum dolor sit amet.</li>
-										<li><i className="fa-solid fa-check pr-2"></i>Lorem ipsum dolor sit amet.</li>
-									</ul>
-								</div>
-							</div>
-							<div className="card-footer d-flex flex-column flex-lg-row align-items-md-center align-items-start justify-content-between">
-								<div>
-									<i className="fa-regular fa-heart p-2"></i><span className="fs-12 p-1"
-										id="votes">17</span><span className="fs-12">Votes</span>
-								</div>
-								<button type="button"
-									className="btn btn-primary "><i
-										className="fa-solid fa-check-to-slot pr-2"></i>Votte</button>
-							</div>
-						</div>
-					</div>
-					<div className="col-xl-4 col-md-6">
-						<div className="card overflow-hidden p-0">
-							<div className="card-header align-items-start border-0">
-								<div>
-									<span className="fs-12 font-weight-bold success">@challengecreator-1</span>
-
-									<h4 className="fs-18 mb-0 pb-2">Challenge Title</h4>
-									<span className="fs-12">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-										eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum
-										suspendisse ultrices gravida. </span>
-									<h4 className="fs-12 text-white pt-3">Rules</h4>
-									<ul className="fs-12">
-										<li><i className="fa-solid fa-check pr-2"></i>Lorem ipsum dolor sit amet.</li>
-										<li><i className="fa-solid fa-check pr-2"></i>Lorem ipsum dolor sit amet.</li>
-									</ul>
-								</div>
-							</div>
-							<div className="card-footer d-flex flex-column flex-lg-row align-items-md-center align-items-start justify-content-between">
-								<div>
-									<i className="fa-regular fa-heart p-2"></i><span className="fs-12 p-1"
-										id="votes">17</span><span className="fs-12">Votes</span>
-								</div>
-								<button type="button"
-									className="btn btn-primary "><i
-										className="fa-solid fa-check-to-slot pr-2"></i>Votte</button>
-							</div>
-						</div>
-					</div>
-					<div className="col-xl-4 col-md-6">
-						<div className="card overflow-hidden p-0">
-							<div className="card-header align-items-start border-0">
-								<div>
-									<span className="fs-12 font-weight-bold success">@challengecreator-1</span>
-
-									<h4 className="fs-18 mb-0 pb-2">Challenge Title</h4>
-									<span className="fs-12">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-										eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum
-										suspendisse ultrices gravida. </span>
-									<h4 className="fs-12 text-white pt-3">Rules</h4>
-									<ul className="fs-12">
-										<li><i className="fa-solid fa-check pr-2"></i>Lorem ipsum dolor sit amet.</li>
-										<li><i className="fa-solid fa-check pr-2"></i>Lorem ipsum dolor sit amet.</li>
-									</ul>
-								</div>
-							</div>
-							<div className="card-footer d-flex flex-column flex-lg-row align-items-md-center align-items-start justify-content-between">
-								<div>
-									<i className="fa-regular fa-heart p-2"></i><span className="fs-12 p-1"
-										id="votes">17</span><span className="fs-12">Votes</span>
-								</div>
-								<button type="button"
-									className="btn btn-primary "><i
-										className="fa-solid fa-check-to-slot pr-2"></i>Votte</button>
-							</div>
-						</div>
-					</div>
-					<div className="col-xl-4 col-md-6">
-						<div className="card overflow-hidden p-0">
-							<div className="card-header align-items-start border-0">
-								<div>
-									<span className="fs-12 font-weight-bold success">@challengecreator-1</span>
-
-									<h4 className="fs-18 mb-0 pb-2">Challenge Title</h4>
-									<span className="fs-12">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-										eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum
-										suspendisse ultrices gravida. </span>
-									<h4 className="fs-12 text-white pt-3">Rules</h4>
-									<ul className="fs-12">
-										<li><i className="fa-solid fa-check pr-2"></i>Lorem ipsum dolor sit amet.</li>
-										<li><i className="fa-solid fa-check pr-2"></i>Lorem ipsum dolor sit amet.</li>
-									</ul>
-								</div>
-							</div>
-							<div className="card-footer d-flex flex-column flex-lg-row align-items-md-center align-items-start justify-content-between">
-								<div>
-									<i className="fa-regular fa-heart p-2"></i><span className="fs-12 p-1"
-										id="votes">17</span><span className="fs-12">Votes</span>
-								</div>
-								<button type="button"
-									className="btn btn-primary "><i
-										className="fa-solid fa-check-to-slot pr-2"></i>Votte</button>
-							</div>
-						</div>
-					</div>
-					<div className="col-xl-4 col-md-6">
-						<div className="card overflow-hidden p-0">
-							<div className="card-header align-items-start border-0">
-								<div>
-									<span className="fs-12 font-weight-bold success">@challengecreator-1</span>
-
-									<h4 className="fs-18 mb-0 pb-2">Challenge Title</h4>
-									<span className="fs-12">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-										eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum
-										suspendisse ultrices gravida. </span>
-									<h4 className="fs-12 text-white pt-3">Rules</h4>
-									<ul className="fs-12">
-										<li><i className="fa-solid fa-check pr-2"></i>Lorem ipsum dolor sit amet.</li>
-										<li><i className="fa-solid fa-check pr-2"></i>Lorem ipsum dolor sit amet.</li>
-									</ul>
-								</div>
-							</div>
-							<div className="card-footer d-flex flex-column flex-lg-row align-items-md-center align-items-start justify-content-between">
-								<div>
-									<i className="fa-regular fa-heart p-2"></i><span className="fs-12 p-1"
-										id="votes">17</span><span className="fs-12">Votes</span>
-								</div>
-								<button type="button"
-									className="btn btn-primary "><i
-										className="fa-solid fa-check-to-slot pr-2"></i>Votte</button>
-							</div>
-						</div>
-					</div>
-					<div className="col-xl-4 col-md-6">
-						<div className="card overflow-hidden p-0">
-							<div className="card-header align-items-start border-0">
-								<div>
-									<span className="fs-12 font-weight-bold success">@challengecreator-1</span>
-=======
-import Link from "next/link";
 import { useEffect, useState } from "react";
 import { create } from "ipfs-http-client";
 import { concat } from "uint8arrays";
 
 const server = create({
-  url: "http://127.0.0.1:5001",
+	url: "http://127.0.0.1:5001",
 });
 
 export default function Votechallenge() {
-  const [challenges, setChallenges] = useState<any[]>([]);
-  const obj = {};
-
-  useEffect(() => {
-    const getData = async () => {
-	let challenges = [];
-      for await (const resultPart of server.files.ls("/")) {
-		let challenge;
-		let vote;
-
-		for await (const cha of server.files.ls(`/${resultPart.name}`)) {
-			const chunks = [];
-			if (cha.name == 'votes') {
-				let votes = await server.files.stat(`/${resultPart.name}/votes`)
-				vote = votes.blocks;
-			} 
-
-			if (cha.name == 'challenge.json') {
-			 for await (const chunk of server.cat(cha.cid)) {
-				chunks.push(chunk);
-				}
-				const data = concat(chunks);
-				challenge = JSON.parse(
-				new TextDecoder().decode(data).toString()
-				);
-			 }
->>>>>>> 2628ba9c72dd443025a995e0488c67507a6c804d
-
-			}
-
-			let challengeData = {
-				challenge: challenge,
-				votes: vote
-			}
-			challenges.push(challengeData);
-		}
-
-	    setChallenges(challenges);
-    };
-    getData();
-  }, []);
+	const [challenges, setChallenges] = useState<any[]>([]);
+	const obj = {};
   
-  console.log("challenges", challenges);
-  return (
-    <>
+	useEffect(() => {
+	  const getData = async () => {
+	  let challenges = [];
+		for await (const resultPart of server.files.ls("/")) {
+		  let challenge;
+		  let vote;
+  
+		  for await (const cha of server.files.ls(`/${resultPart.name}`)) {
+			  const chunks = [];
+			  if (cha.name == 'votes') {
+				  let votes = await server.files.stat(`/${resultPart.name}/votes`)
+				  vote = votes.blocks;
+			  } 
+  
+			  if (cha.name == 'challenge.json') {
+			   for await (const chunk of server.cat(cha.cid)) {
+				  chunks.push(chunk);
+				  }
+				  const data = concat(chunks);
+				  challenge = JSON.parse(
+				  new TextDecoder().decode(data).toString()
+				  );
+			   }
+			  }
+  
+			  let challengeData = {
+				  challenge: challenge,
+				  votes: vote
+			  }
+			  challenges.push(challengeData);
+		  }
+  
+		  setChallenges(challenges);
+	  };
+	  getData();
+	}, []);
+	
+	console.log("challenges", challenges);
+	return (
+<>
       <div className="container-fluid pt-3">
         <div className="row pt-3">
           {challenges.map((camp) => (
@@ -286,5 +97,5 @@ export default function Votechallenge() {
         </div>
       </div>
     </>
-  );
+	);
 }
