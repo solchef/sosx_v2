dead3ba1f738bc68b9d5e2c0f39938f704f4be09
Omar Elweshy <37350174+omarelweshy@users.noreply.github.com>
2022-05-13 19:54:53 +0400

    finish validation

--- src/utils/validateLink.tsx
+++ src/utils/validateLink.tsx
@@ -1,13 +1,17 @@
-export const validLinks = (youtubeURL, tiktokURL) => {
-    const youtube = new RegExp("^http(?:s)?://(?:www.)?(youtube).com/watch?v=([a-zA-Z0-9_]+)", "g")
-    const tiktok = new RegExp("^http(?:s)?://(?:www.)?(youtube).com/watch?v=([a-zA-Z0-9_]+)", "g")
-    if (!youtubeURL && !tiktokURL) {
-        return "One Link Required"
-    } else if (!youtube.test(youtubeURL)) {
-        return "Invalid Youtube Link"
-    } else if (!tiktok.test(tiktokURL)) {
-        return "Invalid TikTok Link"
-    } else {
+export const validLinks = (youtubeURL: string, tiktokURL: string) => {
+    const youtube = /^(?:https?:\/\/)?(?:m\.|www\.)?(?:youtu\.be\/|youtube\.com\/(?:embed\/|v\/|watch\?v=|watch\?.+&v=))((\w|-){11})(?:\S+)?$/
+    const tiktok = /^(?:https?:\/\/)?(?:m\.|www\.|vt\.|)?(tiktok\.com\/)[a-zA-Z0-9!@#$&()\\-`.+_?=,/\"]*$/
+
+    if ((youtubeURL && tiktokURL) ) {
+        if (!tiktokURL.match(tiktok) || !youtubeURL.match(youtube)) {
+            return false
+        }
         return true
+    } else if (tiktokURL.match(tiktok) && !youtubeURL) {
+        return true
+    } else if (youtubeURL.match(youtube) && !tiktokURL) {
+        return true
+    } else {
+        return false
     }
 } 
