2f00b5e16b41fd1ebcd428860d71d1c801c5b1ca
Fanitrade <fanitrade@Fanitrades-iMac.local>
2022-05-23 17:50:51 +0400

    ref

--- src/views/Referral/components/steps/step1.tsx
+++ src/views/Referral/components/steps/step1.tsx
@@ -1,25 +1,33 @@
 const Step1 = () => {
   return (
-    <div className="row">
-      <div className="col-md-6">
-        <div className="title-pink custom-pt">
-          <h3>Step 1</h3>
-          <h3>Get a referral link</h3>
-          <p>
-          Connect a wallet & generate your referral link in the Referral
+    <div className="d-flex flex-column-reverse">
+    <div className="w-100">
+      <div className="title-pink custom-pt mb-5">
+
+      <h3>Step 1</h3>
+
+      <h3>Get a referral link</h3>
+        <p>
+        Connect a wallet & generate your referral link in the Referral
                 section.
-          </p>
-        </div>
+        </p>
       </div>
-      <div className="col-md-6">
-        <img
+    </div>
+    <div
+      className="guide-image justify-items-center"
+      style={{alignSelf: "center"}}
+    >
+    <img
           src="images/step1-referrallink.png"
           data-aos="fade-up"
           alt=""
           className="aos-init aos-animate"
         />
-      </div>
     </div>
+  </div>
+
+
+   
   );
 };
 
--- src/views/Referral/components/steps/step2.tsx
+++ src/views/Referral/components/steps/step2.tsx
@@ -1,26 +1,32 @@
 const Step2 = () => {
   return (
-    <div className="row">
-      <div className="col-md-6">
-        <img
-          src="images/step2invitefriend-img.png" height={100}
-          data-aos="fade-up"
-          alt=""
-          className="aos-init aos-animate"
-        />
-      </div>
-
-      <div className="col-md-6">
-        <div className="title-pink custom-pt">
-          <h3>Step 2</h3>
+    <div className="d-flex flex-column-reverse">
+                <div className="w-100">
+                  <div className="title-pink custom-pt mb-5">
+                  <h3>Step 2</h3>
           <h3>Get a referral link</h3>
           <p>
           Connect a wallet & generate your referral link in the Referral
                 section.
           </p>
-        </div>
-      </div>
-    </div>
+                  </div>
+                </div>
+                <div
+                  className="guide-image justify-items-center"
+                  style={{alignSelf: "center"}}
+                >
+                <img
+          src="images/step2invitefriend-img.png" height={100}
+          data-aos="fade-up"
+          alt=""
+          className="aos-init aos-animate"
+        />
+                </div>
+              </div>
+
+
+
+
   );
 };
 
