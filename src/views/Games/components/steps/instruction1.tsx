46cf41de4c0c9cb93d8d86938efd124d4ccbc5e2
jawmeschege <jawiwy@gmail.com>
2022-05-24 14:13:20 +0400

    add sliders

---
+++ src/views/Games/components/steps/instruction1.tsx
@@ -0,0 +1,30 @@
+const Instruction1 = () => {
+
+    return (
+        <div className="d-flex flex-column-reverse">
+                  <div className="w-100">
+                    <div className="title-pink custom-pt mb-5">
+                      <h3>HOW DOES OX GAMES WORKS</h3>
+                      <p>
+                             First person to complete the voted challenge by the SOSX DAOX community and submit the video wins the prize pool!
+                      </p>
+                    </div>
+                  </div>
+                  <div
+                    className="guide-image justify-items-center"
+                    style={{alignSelf: "center"}}
+                  >
+                    <img
+                      src="/images/popup1.jpg"
+                      data-aos="fade-up"
+                      alt=""
+                      style={{maxHeight:"300px"}}
+                      className="aos-init aos-animate"
+                    />
+                  </div>
+                </div>
+    );
+  };
+  
+  export default Instruction1;
+  
--- src/views/Games/guide.tsx
+++ src/views/Games/guide.tsx
@@ -10,6 +10,8 @@ import Step1 from "./components/steps/step1";
 import Step2 from "./components/steps/step2";
 import Step3 from "./components/steps/step3";
 import Step4 from "./components/steps/step4";
+import Instruction1 from "./components/steps/instruction1";
+import Instruction2 from "./components/steps/instruction2";
 
 const GameGuide = () => {
   const [showGuide, setshowGuide] = useState(false);
@@ -17,12 +19,10 @@ const GameGuide = () => {
   const handleStartGuide = () => setshowGuide(true);
   const [showGameGuide, setShowGameGuide] = useState(false);
   const [step, setStep] = useState(1);
-  const [title, setTitle] = useState('');
-
+  const [title, setTitle] = useState("");
 
   useEffect(() => {
     checkGuideShow();
-          
   }, []);
 
   const checkGuideShow = () => {
@@ -51,56 +51,83 @@ const GameGuide = () => {
   const manageStepShow = () => {
     switch (step) {
       case 1:
-        return <Step1 />;
+        return <Instruction1 />;
       case 2:
-        return <Step2 />;
+        return <Instruction2 />;
       case 3:
         return <Step3 />;
       case 4:
         return <Step4 />;
+      case 5:
+        return <Step3 />;
+      case 6:
+        return <Step4 />;
       default:
         // return <Step1 />;
-        break
+        break;
     }
-
   };
 
   return (
     <>
-    
       <Modal show={showGuide} centered size="lg">
-          <div className="modal-content">
-            <div
-              className="text-dark modal-header"
-              style={{background: "rgb(17, 17, 23)", borderRadius: "10px 10px 0px 0px"}}>
-              OXGAME GUIDE
-              <a href="#" onClick={handleCloseGuide} className="pull-right text-white">
-                <i className="fa fa-close"></i>
-              </a>
-            </div>
-            <div
-              className="modal-body"
-              style={{background: "rgb(17, 17, 23)", borderRadius: "0px 0px 10px 10px"}}
+        <div className="modal-content">
+          <div
+            className="text-dark modal-header"
+            style={{
+              background: "rgb(17, 17, 23)",
+              borderRadius: "10px 10px 0px 0px",
+            }}
+          >
+            OXGAME GUIDE
+            <a
+              href="#"
+              onClick={handleCloseGuide}
+              className="pull-right text-white"
             >
-              {manageStepShow()}
-              <div
-                className="modal-footer guide-popup"
-                style={{ justifyContent: "space-between" }}
-              >
-                <div>
-                  <input onClick={() => pauseGuideShow()} type="checkbox" style={{ marginRight: "10px" }} />
-                  <label>Don't show this again</label>
-                </div>
+              <i className="fa fa-close"></i>
+            </a>
+          </div>
+          <div
+            className="modal-body"
+            style={{
+              background: "rgb(17, 17, 23)",
+              borderRadius: "0px 0px 10px 10px",
+            }}
+          >
+            {manageStepShow()}
+            <div
+              className="modal-footer guide-popup"
+              style={{ justifyContent: "space-between" }}
+            >
+              <div>
+                <input
+                  onClick={() => pauseGuideShow()}
+                  type="checkbox"
+                  style={{ marginRight: "10px" }}
+                />
+                <label>Don't show this again</label>
+              </div>
 
-                <div>
+              <div>
                 {step !== 1 && (
-                  <button onClick={() => setStep(step - 1)} className="btn btn-primary btn-sm m-2">Back</button>
+                  <button
+                    onClick={() => setStep(step - 1)}
+                    className="btn btn-primary btn-sm m-2"
+                  >
+                    Back
+                  </button>
                 )}
-                  <button onClick={() => setStep(step == 4 ? 1 : step + 1)} className="btn btn-primary btn-sm m-2">{step == 4 ? "Start Over" : "NEXT"}</button>
-                </div>
+                <button
+                  onClick={() => setStep(step == 4 ? 1 : step + 1)}
+                  className="btn btn-primary btn-sm m-2"
+                >
+                  {step == 4 ? "Start Over" : "NEXT"}
+                </button>
               </div>
             </div>
           </div>
+        </div>
       </Modal>
     </>
   );
